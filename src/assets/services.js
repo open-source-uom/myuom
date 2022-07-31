d3e5ab06f1fc9a12ae437aa310218b252a4c556c
geoapos <iis20001@uom.edu.gr>
2022-07-31 16:28:08 +0300

    Added services and Services Page
    I made a services.js with all the university services, which I present inside the ServicesPage.jsx and ServicesCard.jsx.

---
+++ src/assets/services.js
@@ -0,0 +1,124 @@
+export const servicesData = [
+  {
+    title: "Γραφείο Διασύνδεσης",
+    url: "https://www.uom.gr/career-office",
+    category: "Γραφείο",
+    imgUrl:
+      "https://www.uom.gr/assets/site/public/nodes/11869/13503-GrafeioDiasyndesis-2.jpg",
+  },
+  {
+    title: "Γραφείο Πρακτικής",
+    url: "http://practice.uom.gr/",
+    category: "Γραφείο",
+    imgUrl:
+      "https://www.uom.gr/assets/site/public/nodes/8621/7799-praktikiaskisikyriafoto-4.jpg",
+  },
+  {
+    title: "Γραφείο Αποφοίτων",
+    url: "https://www.uom.gr/apofitoi",
+    category: "Γραφείο",
+    imgUrl: "https://www.uom.gr/assets/site/content/alumni/ALUMNI_LOGO_GR.PNG",
+  },
+  {
+    title: "Γραφείο Erasmus",
+    url: "https://www.uom.gr/erasmus-office",
+    category: "Γραφείο",
+    imgUrl:
+      "https://www.uom.gr/assets/site/public/nodes/4228/2644-erasmus-header-2.jpg",
+  },
+  {
+    title: "Γραφείο Φυσικής Αγωγής",
+    url: "https://www.uom.gr/gym-office",
+    category: "Γραφείο",
+    imgUrl: "https://pbs.twimg.com/media/C5WlRPKWcAIJ90T.jpg",
+  },
+  {
+    title: "Σύλλογος Φοιτητών Erasmus",
+    url: "http://afroditi.uom.gr/erasmusp",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/68461055_2567378349949617_6059434573815611392_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Rc84N56gO90AX-Qsxue&_nc_ht=scontent-vie1-1.xx&oh=00_AT-7Rz4PU11rgW0wQdhyCBjA7-bYuIxz20aTaJfTyQEaeg&oe=630D9DF3",
+  },
+  {
+    title: "Σύλλογος Φοιτητών ΠΑΜΑΚ",
+    url: "https://www.facebook.com/sfpamak/",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://scontent-vie1-1.xx.fbcdn.net/v/t31.18172-8/332652_231463590242016_1876985924_o.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ZSAYZHqKDzQAX85_d3j&_nc_ht=scontent-vie1-1.xx&oh=00_AT9_00nBjF7KJU0lZdH-StNEdXc8rq_XomRdVxwO-pm45Q&oe=630CB29E",
+  },
+  {
+    title: "TedxUniversityofMacedonia",
+    url: "https://tedxuniversityofmacedonia.com/",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://scontent-vie1-1.xx.fbcdn.net/v/t1.6435-9/31711024_977566345731979_7560213167716958208_n.png?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=JSONUoVbX9YAX-HgqPz&_nc_ht=scontent-vie1-1.xx&oh=00_AT87jYyp_PMMQkMJPOQm_se1Jl-1DhJPARwFeBofo5I_Gw&oe=630A6717",
+  },
+  {
+    title: "AIESEC",
+    url: "https://www.uom.gr/aiesec",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4a/AIESEC-New-Logo1.png",
+  },
+  {
+    title: "Finance Club",
+    url: "https://www.financeclubuom.org/",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLQ86pDFLPNZTJulIeWRCJajirV5WYsCiapFivo0=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    title:
+      "Άλικο ΚΘΒΕ-ΠΑΜΑΚ (πρώην Πολυφωνικά Αναλόγια) & Θεατρικό Εργαστήρι ΚΘΒΕ-ΠΑΜΑΚ",
+    url: "https://www.uom.gr/aliko-kthbe-pamak-prohn-polyfonika-analogia-theatriko-ergasthri-kthbe-pamak",
+    category: "Σύλλογος",
+    imgUrl:
+      "https://www.uom.gr/assets/site/public/nodes/6799/4531-aliko-theatrikoergastiri-kthvepamak-4.jpg",
+  },
+  {
+    title: "Open Source UoM",
+    url: "https://opensourceuom.gitlab.io/",
+    category: "Σύλλογος",
+    imgUrl: "https://opensourceuom.gitlab.io/assets/img/team/avatar.png",
+  },
+  {
+    title: "Λέσχη Κυβερνοασφάλειας Πανεπιστημίου Μακεδονίας",
+    url: "https://csc.uom.gr/",
+    category: "Σύλλογος",
+    imgUrl: "https://csc.uom.gr/wp-content/uploads/2021/02/Myrmidones_02.jpg",
+  },
+  {
+    title: "Γραφείο Περιβαλλοντικής Διαχείρισης ΠαΜακ ",
+    url: "http://www.perivpamak.gr/",
+    category: "Γραφείο",
+    imgUrl:
+      "https://1.bp.blogspot.com/-t4PMium6v6g/X2x6Sz0aAFI/AAAAAAAAD4A/bJNjlS_kYr4lXGQdQBAD4mMdmrWEJ0qjgCNcBGAsYHQ/s2048/Logo%2B%25CE%25A0%25CE%25B5%25CF%2581%25CE%25B9%25CE%25B2%25CE%25B1%25CE%25BB%25CE%25BB%25CE%25BF%25CE%25BD%25CF%2584%25CE%25B9%25CE%25BA%25CE%25AE%25CF%2582.png",
+  },
+  {
+    title:
+      "Επιτροπή Ηθικής και Δεοντολογίας της Έρευνας (Ε.Η.Δ.Ε.) του Πανεπιστημίου Μακεδονίας",
+    url: "https://www.uom.gr/ethics",
+    category: "Υπόλοιπες",
+    imgUrl: "https://www.uom.gr/assets/site/public/nodes/4013/10348-7.png",
+  },
+  {
+    title: "Συνήγορος του Φοιτητή",
+    url: "https://www.uom.gr/synhgoros-toy-foithth-2021-2022",
+    category: "Υπόλοιπες",
+    imgUrl:
+      "https://foititisonline.gr/wp-content/uploads/2018/08/sinigoros-tou-foititi-3.jpg",
+  },
+  {
+    title: "Επιτροπή Ισότητας Φύλων",
+    url: "https://www.uom.gr/eif",
+    category: "Υπόλοιπες",
+    imgUrl: "https://www.uom.gr/assets/site/public/nodes/8310/9033-EIFLOGO.jpg",
+  },
+  {
+    title: "Κέντρο Συμβουλευτικής και Στήριξης Φοιτητών",
+    url: "https://www.uom.gr/student-support",
+    category: "Υπόλοιπες",
+    imgUrl:
+      "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/243911191_4332951503469182_7675468211503498530_n.png?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=kfEkq6pU_AkAX-4YOh3&_nc_ht=scontent-vie1-1.xx&oh=00_AT9OXcwJcH6ifjKyBxP7yVzSMOD548bygy9J12ChMIRx5A&oe=62EB03CF",
+  },
+];
