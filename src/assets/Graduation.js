145107a5e033742a1b1b1bed3082e2f20c0058a3
geoapos <iis20001@uom.edu.gr>
2022-08-04 22:36:27 +0300

    First commit on branch (It's not working)

--- src/assets/Graduation.js
+++ src/assets/Graduation.js
@@ -1,44 +1,40 @@
-const GradData = 
-[
- {
-   "school": "Σχολή Επιστημών Πληροφορίας",
-   "department": "Εφαρμοσμένης Πληροφορικής",
-   "departmentURL": "https://www.uom.gr/dai/apofoithsh"
- },
- {
-   "school": "Σχολή Οικονομικών και Περιφερειακών Σπουδών",
-   "department": "Τμήμα Οικονομικών Επιστημών",
-   "departmentURL": "https://www.uom.gr/eco/dieksagogh-neas-orkomosias" 
- },
-{
-   "school": "Σχολή Οικονομικών και Περιφερειακών Σπουδών",
-   "department": "Τμήμα Βαλκανικών, Σλαβικών & Ανατολικών Σπουδών",
-   "departmentURL": "https://www.uom.gr/11587-anakoinosh-gia-dia-zoshs-orkomosia-toy-tmhmatos" 
-},
- {
-   "school": "Σχολή Επιστημών Διοίκησης Επιχειρήσεων",
-   "department": "Τμήμα Οργάνωσης & Διοίκησης Επιχειρήσεων",
-   "departmentURL": "https://www.uom.gr/ba/anakoinosh-ypobolhs-aithseon-orkomosias-noembrioy-2019"
- },
-{
-   "school": "Σχολή Επιστημών Διοίκησης Επιχειρήσεων",
-   "department": "Τμήμα Λογιστικής και Χρηματοοικονομικής",
-   "departmentURL": "https://www.uom.gr/fin/enhmerosh?tagid=7435"
- },
+import { DEPARTMENTS } from "../assets/DepNames";
+
+const GradData = [
   {
-   "school": "Σχολή Κοινωνικών, Ανθρωπιστικών Επιστημών και Τεχνών",
-   "department": "Τμήμα Διεθνών & Ευρωπαϊκών Σπουδών",
-   "departmentURL": "https://www.uom.gr/ies/orkomosies-apofoitoi" },
+    departmentName: DEPARTMENTS[3],
+    departmentURL: "https://www.uom.gr/dai/apofoithsh",
+  },
   {
-   "school": "Σχολή Κοινωνικών, Ανθρωπιστικών Επιστημών και Τεχνών",
-   "department": "Τμήμα Εκπαιδευτικής & Κοινωνικής Πολιτικής",
-   "departmentURL": "https://www.uom.gr/esp/orkomosies"
- },
+    departmentName: DEPARTMENTS[6],
+    departmentURL: "https://www.uom.gr/eco/dieksagogh-neas-orkomosias",
+  },
   {
-   "school": "Σχολή Κοινωνικών, Ανθρωπιστικών Επιστημών και Τεχνών",
-   "department": "Τμήμα Μουσικής Επιστήμης & Τέχνης",
-   "departmentURL": "https://www.uom.gr/msa/orkomosia-tmhmatos"
- }
-]
+    departmentName: DEPARTMENTS[0],
+    departmentURL:
+      "https://www.uom.gr/11587-anakoinosh-gia-dia-zoshs-orkomosia-toy-tmhmatos",
+  },
+  {
+    departmentName: DEPARTMENTS[7],
+    departmentURL:
+      "https://www.uom.gr/ba/anakoinosh-ypobolhs-aithseon-orkomosias-noembrioy-2019",
+  },
+  {
+    departmentName: DEPARTMENTS[4],
+    departmentURL: "https://www.uom.gr/fin/enhmerosh?tagid=7435",
+  },
+  {
+    departmentName: DEPARTMENTS[1],
+    departmentURL: "https://www.uom.gr/ies/orkomosies-apofoitoi",
+  },
+  {
+    departmentName: DEPARTMENTS[2],
+    departmentURL: "https://www.uom.gr/esp/orkomosies",
+  },
+  {
+    departmentName: DEPARTMENTS[5],
+    departmentURL: "https://www.uom.gr/msa/orkomosia-tmhmatos",
+  },
+];
 
 export default GradData;
--- src/pages/GraduationPage.jsx
+++ src/pages/GraduationPage.jsx
@@ -1,25 +1,55 @@
 import { useEffect, useState, useContext } from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Toast, useToast } from "@chakra-ui/react";
 import GradData from "../assets/Graduation.js";
 import { DepartmentContext } from "../contexts/departmentContext";
+import { useDisclosure } from "@chakra-ui/react";
+import { DEPARTMENTS } from "../assets/DepNames";
 
 function GraduationPage() {
   const { depName } = useContext(DepartmentContext);
-  const [Gdata, setGdata] = useState("");
+  const [Gdata, setGdata] = useState([]);
 
   useEffect(() => {
-    const GradData = getdataByDepartment(depName);
-    setGdata(GradData);
+    const filteredData = getdataByDepartment(depName);
+    setGdata(filteredData);
   }, [depName]);
 
-  const getdataByDepartment = (depName) =>
-    GradData.find((Gdata) => Gdata.department === depName);
+  const getdataByDepartment = (depName) => {
+    let findGradData = GradData.find(
+      (gData) => gData.departmentName === depName
+    );
+    return findGradData;
+  };
+
+  {
+    /*const showToast = () =>
+    toast({
+      title: "Account created.",
+      description: "We've created your account for you.",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    });*/
+  }
+
+  // function redirectToPage() {
+  //   //console.log(Gdata.departmentURL);
+  //   //window.location.replace(JSON.stringify(Gdata.departmentURL));
+  // }
 
   return (
-    <Heading textAlign="center" marginTop="50px">
-      Redirecting to {Gdata.departmentURL}...
-      {window.location.replace(Gdata.departmentURL)}
-    </Heading>
+    <>
+      {!depName ? (
+        <Heading textAlign="center" marginTop="50px">
+          Select department from settings
+        </Heading>
+      ) : (
+        <Heading textAlign="center" marginTop="50px">
+          Redirecting to {Gdata.departmentURL}...
+          {window.location.replace(Gdata.departmentURL)}
+        </Heading>
+      )}
+    </>
   );
 }
 
