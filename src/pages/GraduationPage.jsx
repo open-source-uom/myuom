016eb2b134d574b049bba6e0ff4d5058ce7b35b2
socratesomiliadis <s.omiliadis@gmail.com>
2022-08-05 12:06:40 +0300

    Fixes Temp

--- src/pages/GraduationPage.jsx
+++ src/pages/GraduationPage.jsx
@@ -1,40 +1,42 @@
-import React, { useContext, useEffect } from 'react'
-import { DepartmentContext } from '../contexts/departmentContext'
-import { GradData } from '../assets/Graduation'
-import { Heading } from "@chakra-ui/react";
-import { useToast } from '@chakra-ui/react'
+import React, { useContext, useEffect } from "react";
+import { DepartmentContext } from "../contexts/departmentContext";
+import { GradData } from "../assets/Graduation";
+import { Heading, useToast, Box } from "@chakra-ui/react";
 export default function GraduationPage() {
+  const { depName } = useContext(DepartmentContext);
+  const toast = useToast();
 
-  const { depName } = useContext(DepartmentContext)
-  const toast = useToast()
+  //Toast and Window.open action moved to useEffect so that they run on client-side and dont create any problems.
 
   useEffect(() => {
-    console.log(GradData.get(depName));
-  }, [depName])
-
-
-  const showToast = () => {
-    toast({
-      title: 'Department Not Selected',
-      description: "Please Select a Department From The Settings",
-      status: 'error',
-      duration: 5000,
-      isClosable: true,
-      position:'top-right'
-    })
-  }
+    if (!depName) {
+      toast({
+        title: "Department Not Selected",
+        description: "Please Select a Department From The Settings",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top-right",
+      });
+    }
+    if (GradData.get(depName)) window.open(GradData.get(depName));
+  }, [depName]);
 
   //<Text align={'center'}> SELECT DEPARTMENT</Text>
   return (
-    <>
-      {
-        GradData.get(depName) ? <Heading textAlign="center" marginTop="50px">
-          Redirecting to <a href={GradData.get(depName)} target={'_blank'} rel="noreferrer">{GradData.get(depName)}</a>
-          {window.open(GradData.get(depName))}
-        </Heading> : showToast()
-      }
-    </>
-
-  )
+    <Box>
+      {GradData.get(depName) ? (
+        <Heading textAlign="center" marginTop="50px">
+          Redirecting to
+          <a href={GradData.get(depName)} target="_blank" rel="noreferrer">
+            {GradData.get(depName)}
+          </a>
+        </Heading>
+      ) : (
+        <Heading textAlign="center" marginTop="50px">
+          Please select a department from settings.
+        </Heading>
+      )}
+    </Box>
+  );
 }
-
