005def3ca05f5c42c35450c81d55ac517bf2a9df
geoapos <iis20001@uom.edu.gr>
2022-08-08 19:30:42 +0300

    Added Exams and Semester Schedules

---
+++ src/pages/SchedulePage.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect } from "react";
+import { DepartmentContext } from "../contexts/departmentContext";
+import { schedulesData } from "../assets/ScheduleLink";
+import { Heading, Box } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+export default function SchedulePage({ examsProp, semesterProp }) {
+  const { depName } = useContext(DepartmentContext);
+  const navigate = useNavigate();
+
+  const redirectTo = (link) => {
+    window.open(link);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    let scheduleData = schedulesData.get(depName);
+
+    if (examsProp) {
+      redirectTo(scheduleData.exam);
+    }
+
+    if (semesterProp) {
+      redirectTo(scheduleData.semester);
+    }
+  }, [depName]);
+
+  //<Text align={'center'}> SELECT DEPARTMENT</Text>
+  return (
+    <Box>
+      {schedulesData.get(depName) ? (
+        <Heading textAlign="center" marginTop="50px">
+          Redirecting to
+        </Heading>
+      ) : (
+        <Heading textAlign="center" marginTop="50px">
+          Please select a department from settings.
+        </Heading>
+      )}
+    </Box>
+  );
+}
