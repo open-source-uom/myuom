c47c9cf8f830076c1949df1b02a041243adb36f7
christos fanourios patsouras <xristos.patsoyras@gmail.com>
2022-08-11 19:31:29 +0300

    Fixes #50

--- src/pages/FAQSettingsPage.jsx
+++ src/pages/FAQSettingsPage.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import {
   Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionIcon,
-  AccordionPanel,
   Box,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import FAQCard from "../components/FAQCard";
-import questionsForFAQ from "../assets/questionsForFAQ";
+import data from "../assets/questionsForFAQ";
 
 export default function FAQSettingsPage() {
   return (
-    <Box bg="white" textAlign={"center"}>
-      <Text as="h1" fontSize="3.5ch" fontWeight="bold" marginBottom="1em">
-        Συχνές Ερωτήσεις
-      </Text>
-      <Text>
-        Εδώ θα βρείτε απαντήσεις σε ερωτήσεις που ενδέχεται να σας ενδιαφέρουν.
-      </Text>
-      <Accordion allowToggle>
-        {questionsForFAQ.map((data) => {
-          return <FAQCard data={data} key={data.id} />;
-        })}
-      </Accordion>
-    </Box>
+      <Box textAlign={"center"}>
+        <Box textAlign="center" marginBottom="1em">
+          <Text as="h1" fontSize="3.5ch" fontWeight="bold" marginBottom="1em" paddingTop="10px">
+            Συχνές Ερωτήσεις
+          </Text>
+          <Text padding="15px">
+            Εδώ θα βρείτε απαντήσεις σε ερωτήσεις που ενδέχεται να σας ενδιαφέρουν.
+          </Text>
+        </Box>
+        <Accordion allowToggle>
+          {data.map((data) => {
+            return <FAQCard data={data} key={data.question} />;
+          })}
+        </Accordion>
+      </Box>
   );
 }
