e8011f3bc797c5454a0f1dbe3f4fdb5df35dc192
socratesomiliadis <s.omiliadis@gmail.com>
2022-08-12 11:46:15 +0300

    Finished homepage dark mode styling

--- src/components/MenuBox.jsx
+++ src/components/MenuBox.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import {
   Box,
-  Center,
   useColorModeValue,
   Heading,
-  Stack,
-  Image,
+  GridItem,
+  Flex,
   useToast,
 } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
 import { DepartmentContext } from "../contexts/departmentContext";
-import { DEPARTMENTS } from '../assets/DepNames'
-
+import { DEPARTMENTS } from "../assets/DepNames";
 
 export default function MenuBox({ category }) {
-  const { title, imgUrl, route, isExternal, requireSelection } = category;
+  const { title, iconSVG, route, span, isExternal, requireSelection } =
+    category;
   const { depName } = React.useContext(DepartmentContext);
   const toast = useToast();
   let condition = requireSelection && !DEPARTMENTS.includes(depName);
@@ -30,56 +29,114 @@ export default function MenuBox({ category }) {
   };
 
   React.useEffect(() => {
-    if (requireSelection)
-      console.log(title);
-  }, [])
+    if (requireSelection) console.log(title);
+  }, []);
 
   const handleSelection = () => {
     if (requireSelection && !depName) {
-        toast({
-          title: "Department Not Selected",
-          description: "Please Select a Department From The Settings",
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-          position: "bottom",
-        });
-      } else {
-        handleNavigation();
-      }
+      toast({
+        title: "Department Not Selected",
+        description: "Please Select a Department From The Settings",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    } else {
+      handleNavigation();
+    }
   };
 
   return (
-    <Center py={12} m={3} onClick={handleSelection}>
-      <Box
-        role={"group"}
-        p={6}
-        maxW={"480px"}
-        w={"full"}
-        bg={useColorModeValue(lightModeColour, darkModeColour)}
-        boxShadow={"2xl"}
-        rounded={"lg"}
-        pos={"relative"}
-        zIndex={1}
-        _hover={{
-          bgColor: "blue.600",
-        }}
+    <GridItem
+      cursor="pointer"
+      onClick={handleSelection}
+      colSpan={span}
+      role={"group"}
+      border="2px"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+      borderColor={useColorModeValue("#0050e0", "#f3f3f3")}
+      className={`menu-box span-${span}`}
+      rounded="0.75rem"
+      p={{ sm: 2, md: 4, lg: 6 }}
+    >
+      <Flex
+        w="100%"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-between"
       >
-        <Box rounded={"lg"} mt={10} pos={"relative"} height={"230px"}>
-          <Image
-            rounded={"lg"}
-            height={"full"}
-            width={"full"}
-            objectFit={"cover"}
-            src={imgUrl}
-          />
+        <Box
+          minW={{ sm: "32px", md: "48px", lg: "64px" }}
+          maxW={{ sm: "32px", md: "48px", lg: "64px" }}
+          className={`svg-container ${useColorModeValue(
+            "light-mode-svg",
+            "dark-mode-svg"
+          )}`}
+        >
+          {iconSVG}
         </Box>
-        <Stack pt={10} align={"center"}>
-          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-            {title} {category.isExternal && <ExternalLinkIcon />}
-          </Heading>
-        </Stack>
-      </Box>
-    </Center>
+        <Box
+          minW={{ sm: "18px", md: "24px", lg: "30px" }}
+          maxW={{ sm: "18px", md: "24px", lg: "30px" }}
+          className={`svg-container ${useColorModeValue(
+            "light-mode-svg",
+            "dark-mode-svg"
+          )}`}
+        >
+          {isExternal ? (
+            <svg
+              className="stroke-svg"
+              width="100%"
+              viewBox="0 0 10 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.873535 9L8.91951 1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M0.873535 1H8.91951V9"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="stroke-svg"
+              width="120%"
+              viewBox="0 0 11 11"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 5.66666H10.0575"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M5.52881 0.999985L10.0575 5.66665L5.52881 10.3333"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          )}
+        </Box>
+      </Flex>
+      <Heading
+        fontSize={{ sm: 13, md: 16, lg: 26, xl: 38 }}
+        color={useColorModeValue("black", "#f3f3f3")}
+        w="100%"
+        fontWeight={{ sm: 600, lg: 500 }}
+        fontFamily="Syne"
+      >
+        {title}
+      </Heading>
+    </GridItem>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { Categories } from "../assets/categories";
 import MenuBox from "../components/MenuBox";
-import { SimpleGrid } from "@chakra-ui/react";
+import { Flex, Box, Grid } from "@chakra-ui/react";
+import { useColorModeValue } from "@chakra-ui/react";
 
 export default function HomePage() {
   return (
-    <SimpleGrid columns={{ sm: 2, md: 2, lg: 3 }}>
-      {Categories.map((category) => (
-        <MenuBox category={category} key={category.title} />
-      ))}
-    </SimpleGrid>
+    <Flex
+      direction="column"
+      align="center"
+      justify="start"
+      w="100%"
+      h="100%"
+      pt="3rem"
+      gap="3rem"
+      px={{ sm: "1rem", md: "0px" }}
+    >
+      <Box
+        bg={useColorModeValue("#0050e0", "#f3f3f3")}
+        w={{ sm: "100%", md: "90%", lg: "80%", "2xl": "60%", "3xl": "50%" }}
+        className="home-page-box"
+        rounded="0.75rem"
+      ></Box>
+      <Grid
+        className="home-grid"
+        w={{ sm: "100%", md: "90%", lg: "80%", "2xl": "60%", "3xl": "50%" }}
+        gap={{ sm: 4, md: 6, lg: 8 }}
+      >
+        {Categories.map((category) => (
+          <MenuBox category={category} key={category.title} />
+        ))}
+      </Grid>
+    </Flex>
   );
 }
