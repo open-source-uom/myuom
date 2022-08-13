090edea18a884a39aee63bb5606e6a4ee395fc64
socratesomiliadis <s.omiliadis@gmail.com>
2022-08-13 19:00:22 +0300

    Added bottom nav bar for mobile devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ export default function Header() {
         alignItems="center"
         zIndex={99}
         justifyContent="space-between"
-        padding={2}
+        padding="1rem"
       >
         <Flex
           w={{ sm: "100%", lg: "fit-content" }}
@@ -57,7 +57,7 @@ export default function Header() {
               src={myUOMLogo}
               onClick={goToHomePage}
               cursor="pointer"
-              w={{ sm: "60px", xl: "80px" }}
+              w={{ sm: "40px", xl: "60px" }}
             />
             <Box
               display={{ sm: "block", lg: "none" }}
@@ -99,7 +99,7 @@ export default function Header() {
             gap={1}
           >
             <Heading
-              fontSize={{ sm: 26, lg: 30, xl: 38 }}
+              fontSize={{ sm: 32, lg: 34 }}
               color={useColorModeValue("black", "#f3f3f3")}
               fontWeight={500}
               fontFamily="Syne"
@@ -138,6 +138,46 @@ export default function Header() {
         <Box display={{ sm: "none", lg: "block" }}>
           <MenuButton />
         </Box>
+      </Flex>
+      <Flex
+        w="100%"
+        paddingX="20px"
+        zIndex={99}
+        height="60px"
+        alignItems="center"
+        justifyContent="space-around"
+        backgroundColor={useColorModeValue(
+          "rgba(255, 255, 255, 1)",
+          "rgba(0, 0, 0, 1)"
+        )}
+        display={{ sm: "flex", lg: "none" }}
+        position="fixed"
+        bottom={0}
+      >
+        <Box onClick={goToHomePage}>
+          <svg
+            className="home-svg"
+            width="28"
+            height="30.8"
+            viewBox="0 0 20 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 8L10 1L19 8V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V8Z"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              stroke={useColorModeValue("#000", "#f3f3f3")}
+            />
+            <path
+              d="M7 21V11H13V21"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              stroke={useColorModeValue("#000", "#f3f3f3")}
+            />
+          </svg>
+        </Box>
+        <MenuButton />
       </Flex>
       <Outlet />
     </>
