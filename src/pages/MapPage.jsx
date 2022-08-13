9daa7944f7cb20e15ff4cbcbc4b500944763e6a4
George David Apostolidis <iis20001@uom.edu.gr>
2022-08-13 18:17:40 +0300

    First commit for map

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import { Option } from "react";
+
+
+function MapPage() {
+    return (
+        <Box align="center" marginTop="1em">
+            <Option />
+        </Box>
+    );
+}
+
+export default MapPage;
