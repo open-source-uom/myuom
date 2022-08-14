968e0f141db4eb9d816387f883d7eb60d0b0f69c
Georgios Fakidis <geonfak76@gmail.com>
2022-08-14 21:37:10 +0300

    Created today's menu component and put it in restaurant page
    The function that determines whether to display this lunch dinner or next day's lunch might need some optimization, i have written a test for personal use where do i upload it? Also it includes an interval every 30 seconds the id of which i log for anyone that wants to clearInterval() it.

---
+++ src/components/TodaysMenu.jsx
@@ -0,0 +1,154 @@
+import React, { useState } from "react";
+import {
+  Flex,
+  ListItem,
+  Text,
+  UnorderedList,
+  useColorModeValue,
+  chakra,
+} from "@chakra-ui/react";
+import data from "../assets/DailyMenu.json";
+import { useEffect } from "react";
+const CURRENTLY_LUNCH = "gevma";
+const CURRENTLY_DINNER = "deipno";
+const CURRENTLY_NEXT_LUNCH = "gevma epomenhs";
+
+export default function TodaysMenu() {
+  const [isLunch, setIsLunch] = useState(true);
+  const [foodMenu, setFoodMenu] = useState(null);
+  const lettersColor = useColorModeValue("white", "black");
+
+  function getNextMeal(curr_date) {
+    const now = curr_date;
+    const dayNum = now.getDay();
+    const isWeekDay = !(dayNum === 6 || dayNum === 0);
+
+    const [endingLunchHour, endingLunchMinutes] = [
+      isWeekDay ? 16 : 15,
+      isWeekDay ? 0 : 30,
+    ];
+    const [endingDinnerHour, endingDinnerMinutes] = [20, 0];
+
+    const [hours, minutes] = [now.getHours(), now.getMinutes()];
+    if (
+      hours < endingLunchHour ||
+      (hours === endingLunchHour && minutes <= endingLunchMinutes)
+    ) {
+      return CURRENTLY_LUNCH;
+    }
+
+    if (
+      hours < endingDinnerHour ||
+      (hours == endingDinnerHour && minutes <= endingDinnerMinutes) ||
+      (hours == endingLunchHour && minutes > endingLunchMinutes)
+    ) {
+      return CURRENTLY_DINNER;
+    }
+
+    return CURRENTLY_NEXT_LUNCH;
+  }
+  function checkTimeSetNextMeal() {
+    const menuToDisplay = getNextMeal(new Date());
+    let todayOrNextDay = 0;
+    if (menuToDisplay === CURRENTLY_LUNCH) {
+      setIsLunch(true);
+    } else if (menuToDisplay === CURRENTLY_DINNER) {
+      setIsLunch(false);
+    } else if (menuToDisplay === CURRENTLY_NEXT_LUNCH) {
+      //next day's lunch
+      setIsLunch(true);
+      todayOrNextDay = 1;
+    }
+    setFoodMenu(getTodaysRestaurantMenu(todayOrNextDay));
+  }
+  useEffect(() => {
+    checkTimeSetNextMeal();
+    const interval = setInterval(checkTimeSetNextMeal, 30 * 1000);
+    console.log(interval);
+    return () => clearInterval(interval);
+  }, []);
+
+  function getTodaysRestaurantMenu(offsetDays) {
+    const days = [
+      "ΚΥΡΙΑΚΗ",
+      "ΔΕΥΤΕΡΑ",
+      "ΤΡΙΤΗ",
+      "ΤΕΤΑΡΤΗ",
+      "ΠΕΜΠΤΗ",
+      "ΠΑΡΑΣΚΕΥΗ",
+      "ΣΑΒΒΑΤΟ",
+    ];
+    //2022, 7, 14
+    const dayNum = new Date().getDay();
+    const dayName = days[dayNum + (offsetDays % 7)];
+    const todaysTotalMenu = data
+      .filter((dayMenu) => {
+        return dayMenu.day === dayName;
+      })
+      .pop();
+
+    if (isLunch) {
+      return {
+        mainDish: todaysTotalMenu.gevmaKirios,
+        specialDish: todaysTotalMenu.gevmaEidiko,
+        salad: todaysTotalMenu.gevmaSalata,
+        dessert: todaysTotalMenu.gevmaEpidorpio,
+      };
+    }
+
+    return {
+      mainDish: todaysTotalMenu.deipnoKirios,
+      specialDish: todaysTotalMenu.deipnoEidiko,
+      salad: todaysTotalMenu.deipnoSalata,
+      dessert: todaysTotalMenu.deipnoEpidorpio,
+    };
+  }
+  return (
+    <Flex
+      flexDirection={"column"}
+      color={lettersColor}
+      marginTop={"1rem"}
+      marginLeft={"1rem"}
+    >
+      <Text fontWeight={"bold"} marginBottom="1rem">
+        Σημερινό Μενού:
+      </Text>
+
+      <Text as="span">{isLunch ? "Γεύμα" : "Δείπνο"}:</Text>
+
+      {foodMenu ? <FoodMenuList {...foodMenu} /> : <div>Loading...</div>}
+    </Flex>
+  );
+}
+
+function FoodMenuList({ mainDish, specialDish, salad, dessert }) {
+  console.log(mainDish, specialDish, salad, dessert);
+  return (
+    <UnorderedList>
+      <ListItem>
+        <Text as="span" noOfLines={[]}>
+          <chakra.u marginRight={"1rem"}>Κυρίως:</chakra.u>
+          {mainDish}
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text as="span">
+          <chakra.u marginRight={"1rem"}>Χορτοφαγικό:</chakra.u>
+          {specialDish}
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text as="span">
+          <chakra.u marginRight={"1rem"}>Σαλάτα:</chakra.u>
+          {salad}
+        </Text>
+      </ListItem>
+      <ListItem>
+        <Text as="span">
+          <chakra.u marginRight={"1rem"}>Γλυκό:</chakra.u>
+          {dessert}
+        </Text>
+      </ListItem>
+    </UnorderedList>
+  );
+}
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 import Menu from "../components/Menu";
 import data from "../assets/DailyMenu.json";
-import { Accordion, Box, Image, Text } from "@chakra-ui/react";
+import {
+  Accordion,
+  Box,
+  Flex,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import Schedule from "../components/Schedule";
 import dinner from "../assets/dinner.png";
+import TodaysMenu from "../components/TodaysMenu";
 
 export default function RestauranPage() {
   return (
-    <Box textAlign={"center"}>
-      <Box align="center">
-        <Text fontSize="30px" fontWeight="bold" fontFamily="cursive">
-          Menu Εστιατορίου
-        </Text>
-        <Image src={dinner} width="70px" height="70px" borderRadius="full" />
+    <Flex direction="column" align="center">
+      <Box
+        bg={useColorModeValue("#0050e0", "#f3f3f3")}
+        w={{ sm: "90%", md: "90%", lg: "80%", "2xl": "60%", "3xl": "50%" }}
+        className="home-page-box"
+        rounded="0.75rem"
+        h={"18rem"}
+        mx={"1rem"}
+        py={"0.5rem"}
+      >
+        <TodaysMenu />
       </Box>
-      <Accordion allowToggle>
-        {data.map((data) => {
-          return <Menu data={data} key={data.day} />;
-        })}
-        <Schedule />
-      </Accordion>
-    </Box>
+      <Box textAlign={"center"}>
+        <Box align="center">
+          <Text fontSize="30px" fontWeight="bold" fontFamily="cursive">
+            Menu Εστιατορίου
+          </Text>
+          <Image src={dinner} width="70px" height="70px" borderRadius="full" />
+        </Box>
+        <Accordion allowToggle>
+          {data.map((data) => {
+            return <Menu data={data} key={data.day} />;
+          })}
+          <Schedule />
+        </Accordion>
+      </Box>
+    </Flex>
   );
 }
