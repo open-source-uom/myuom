432716c0588ff38f96a6ff5dff24e629a286e147
geoapos <iis20001@uom.edu.gr>
2022-08-18 16:12:32 +0300

    Finished LibraryPage styling

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -1,117 +1,137 @@
 import React from "react";
-import { Flex, Box, Text, Button, Image, HStack } from "@chakra-ui/react";
-import { TimeIcon, PhoneIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Box,
+  Text,
+  Button,
+  Image,
+  HStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+import {
+  TimeIcon,
+  PhoneIcon,
+  ExternalLinkIcon,
+  ArrowForwardIcon,
+} from "@chakra-ui/icons";
 
 export default function LibraryPage() {
-  //thelw sto desktop na mikrynei h eikona kai na mpoun ta koutia dipla dipla
   return (
     <Flex flexDirection="column" alignItems="center">
-      <Image
-        src="https://www.uom.gr/assets/site/public/nodes/4021/2038-LibraryLogo_4slider-2.jpg"
-        alt="uom-library-logo"
-        // width="70%"
-        height="20%"
-        alignSelf={"center"}
-        objectFit={"contain"}
-        px="1rem"
-      />
       {/* Wrapper container */}
       <Flex
         textAlign="center"
         flexDirection={{ base: "column", lg: "row" }}
+        columnGap={"3rem"}
         alignItems="center"
         justifyContent={"center"}
         width="100%"
+        height={{ lg: "100vh" }}
       >
         {/* Ωράριο */}
         <Box
           border="2px"
           borderRadius="1rem"
-          borderColor="grey.200"
-          marginRight={{ base: "0", lg: "1rem" }}
+          bg={useColorModeValue("#0050e0", "#f3f3f3")}
+          borderColor={useColorModeValue("#0050e0", "#f3f3f3")}
           marginBottom={{ base: "1rem", lg: "0" }}
           marginTop="1rem"
-          px="1.5rem"
+          px="0.75rem"
           py="1.5rem"
-          width={{ base: "initial", lg: "40%" }}
-          height={{ base: "initial", lg: "40%!important" }}
+          width="max-content"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          height={{ lg: "30%" }}
         >
           <Flex
-            fontWeight="bold"
-            flexDirection={"column"}
+            flexDirection={"row"}
             alignItems="center"
-            rowGap={{ base: "0.5rem", lg: "1.5rem" }}
+            fontFamily="Syne"
+            color={useColorModeValue("#f3f3f3", "black")}
           >
-            <HStack
-              marginBottom={"1rem"}
-              paddingBottom="0.5rem"
-              px="1rem"
-              border={"solid 2px "}
-              borderTop={"none"}
-            >
-              <Text as="span">Ωράριο</Text>
-              <TimeIcon w={39} h={39} />
-            </HStack>
-            <Text> 04-29/07/2022 & 22-26/08/2022 </Text>
-
-            <Text> Δευτέρα - Τετάρτη: 08:00-17:00 </Text>
-            <Text> Τετάρτη - Παρασκευή: 08:00-14:30 </Text>
-            <Text> Σάββατο: ΚΛΕΙΣΤΑ </Text>
+            <TimeIcon w={39} h={39} />
+            <Box mx="1rem">
+              <Text as="span" fontWeight={"bold"}>
+                Δευ-Παρ
+              </Text>{" "}
+              <br />
+              8:00 - 20:00
+            </Box>
+            <Box mx="1rem">
+              <Text as="span" fontWeight={"bold"}>
+                Σάββατο
+              </Text>{" "}
+              <br />
+              9.00-15.00
+            </Box>
+            <Box mx="1rem">
+              <Text as="span" fontWeight={"bold"}>
+                Κυριακή
+              </Text>{" "}
+              <br />
+              κλειστά
+            </Box>
           </Flex>
         </Box>
         {/* Επικοινωνία  */}
         <Box
           border="2px"
           borderRadius="1rem"
-          borderColor="grey.200"
-          marginLeft={{ base: "0", lg: "1rem" }}
-          marginTop={"1rem"}
-          marginBottom={"1rem"}
-          py="2rem"
-          px="3rem"
-          width={{ base: "initial", lg: "40%" }}
-          height={{ base: "initial", lg: "40%" }}
+          borderColor={useColorModeValue("#0050e0", "#f3f3f3")}
+          bg={useColorModeValue("#0050e0", "#f3f3f3")}
+          marginBottom={{ base: "1rem", lg: "0" }}
+          marginTop="1rem"
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          py="1rem"
+          px="0.75rem"
+          width="max-content"
+          height={{ lg: "30%" }}
         >
-          {/* Ligo doubt pws akrivws voithaei o TK kai h odos  */}
           <Flex
-            fontWeight="bold"
-            flexDirection={"column"}
+            mx="1rem"
+            flexDirection={"row"}
             alignItems="center"
             rowGap={"0.75rem"}
+            justifyContent={"flex-start"}
+            columnGap={"1rem"}
+            color={useColorModeValue("#f3f3f3", "black")}
+            fontFamily="Syne"
           >
-            <HStack
-              marginBottom={"1rem"}
-              paddingBottom="0.5rem"
-              px="1rem"
-              border={"solid 2px "}
-              borderTop={"none"}
-            >
-              <Text as="span">Επικοινωνία</Text>
-              <PhoneIcon w={39} h={39} />
-            </HStack>
-            <Text> Ημιώροφος </Text>
-            {/* <Text> Εγνατίας, 156 </Text>
-            <Text> ΤΚ: 54636 </Text> pio poly san subtext */}
-            <Text textDecoration={"underline"}> Τηλέφωνα Επικοινωνίας: </Text>
-            <Text> (2310) 891751</Text>
-            <Text> (2310) 891752 </Text>
-            <Text> (2310) 891118 </Text>
-            <Text> (2310) 891137</Text>
-            <Button
-              // xreiazetai to !important logw VStack mlln
-              marginTop="1rem !important"
-              textAlign="center"
-              colorScheme="green"
-              variant="solid"
-              onClick={(e) => {
-                window.open("https://www.lib.uom.gr/index.php/el/");
-              }}
-              justifyContent="center"
-            >
-              Ιστοσελίδα Βιβλιοθήκης
-            </Button>
+            <PhoneIcon w={39} h={39} />
+
+            <Text fontWeight="bold">
+              {" "}
+              (2310) <br />
+            </Text>
+            <Text>
+              891751 <br />
+              891752
+            </Text>
+            <Text>
+              {" "}
+              891118 <br /> 891137{" "}
+            </Text>
+            <Text fontWeight="bold"> Ημιώροφος </Text>
           </Flex>
         </Box>
+        <Button
+          color={useColorModeValue("#0050e0", "#f3f3f3")}
+          variant="ghost"
+          fontWeight="bold"
+          fontFamily="Syne"
+          fontSize={{ sm: 16, md: 18, lg: 20 }}
+          rightIcon={<ArrowForwardIcon w={6} h={6} />}
+          onClick={(e) => {
+            window.open("https://www.lib.uom.gr/index.php/el/");
+          }}
+          justifyContent="center"
+        >
+          Ιστοσελίδα Βιβλιοθήκης
+        </Button>
       </Flex>
     </Flex>
   );
