f760766b3e262ea412dc8964ffddef119fe7b74b
konkos <kon_kostas@windowslive.com>
2022-09-01 19:15:44 +0300

    Change Cache Policy

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,14 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
-);
+self.addEventListener('install', (event) => {
+  const cacheKey = 'myUoMCache_V1';
 
-workbox.routing.registerRoute(
-  ({ request }) => request.destination === "image",
-  new workbox.strategies.NetworkFirst()
-);
+  event.waitUntil(caches.open(cacheKey).then((cache) => {
+    // Add all the assets in the array to the 'MyFancyCacheName_v1'
+    // `Cache` instance for later use.
+    return cache.addAll([
+      '/css/global.bc7b80b7.css',
+      '/css/home.fe5d0b23.css',
+      '/js/home.d3cc4ba4.js',
+      '/js/jquery.43ca4933.js'
+    ]);
+  }));
+});
