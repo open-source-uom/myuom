09e3d22529a5ebfe968761f7603b5f9827788155
geoapos <iis20001@uom.edu.gr>
2022-09-18 13:26:52 +0300

    First test commit

---
+++ src/assets/projectMembers.js
@@ -0,0 +1,58 @@
+const projectMembers = [
+  {
+    name: "Γεώργιος Δαυίδ Αποστολίδης",
+    socialMedia: "",
+    email: "geoapostolidis999@gmail.com",
+    img: "https://media-exp1.licdn.com/dms/image/C4E03AQF57CzUbeaM7g/profile-displayphoto-shrink_200_200/0/1657542475385?e=1669248000&v=beta&t=SyC0fFEvh71ZSUtl8JFzHxfOEoB-PSVlvFPLUvLuRug",
+  },
+  {
+    name: "Ευστάθιος Ιωσηφίδης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Χρήστος Φανούριος Πατσούρας",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Κωνσταντίνος Κωνσταντινίδης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Ευάγγελος Δάβιος",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Γεώργιος Φακίδης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Πάρης Μπακάλης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Σωκράτης Ομιλιάδης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+  {
+    name: "Πρόδρομος Νάσης",
+    socialMedia: "",
+    email: "",
+    img: "https://media-exp1.licdn.com/dms/image/C4D03AQH1L7p-7_FvEA/profile-displayphoto-shrink_200_200/0/1577614383241?e=1669248000&v=beta&t=ktUc2NpwPmDlNohHvzg-jjBSCXDKxA3RhTqW6_I1aPE",
+  },
+];
+
+export default projectMembers;
---
+++ src/components/ProjectMembersCard.jsx
@@ -0,0 +1,98 @@
+import {
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Box,
+  Button,
+  useDisclosure,
+  Image,
+  Text,
+  VStack,
+  useColorModeValue,
+  Flex,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+export default function ProjectMembersCard({ data }) {
+  const SelectBorderColor = () => {
+    return useColorModeValue("#0050e0", "#f3f3f3");
+  };
+
+  const SelectItemColor = () => {
+    return useColorModeValue("black", "white");
+  };
+
+  return (
+    <Box
+      w="95%"
+      borderRadius="20"
+      overflow="hidden"
+      border="2px"
+      borderColor={SelectBorderColor()}
+    >
+      <AccordionItem border="none" w="100%">
+        <Flex>
+          <AccordionButton
+            display="flex"
+            direction="row"
+            alignItems="center"
+            justifyContent="start"
+            _hover={{ bg: "transparent" }}
+            w="100%"
+            h="100%"
+            outline="none"
+            textAlign="left"
+            bgColor="transparent"
+            color={SelectItemColor()}
+            fontFamily="Syne"
+            border="none"
+            alt="profPic"
+            overflow="hidden"
+            gap={3}
+          >
+            <Image
+              src={data.img}
+              width="60px"
+              height="60px"
+              borderRadius="full"
+            />
+            <Text
+              w="100%"
+              display="flex"
+              direction="row"
+              alignItems="center"
+              justifyContent="start"
+              fontWeight="bold"
+              fontSize={{ sm: 14, md: 16, lg: 18 }}
+            >
+              {data.name}
+            </Text>
+            <AccordionIcon />
+          </AccordionButton>
+        </Flex>
+        <AccordionPanel bgColor="transparent" pb={0} textAlign="center">
+          <Flex direction="column" alignItems="start" fontFamily="Syne">
+            <Text fontWeight="bold" fontSize={{ sm: 12, md: 13, lg: 15 }}>
+              Social Media: {data.socialMedia}
+            </Text>
+            <Text fontWeight="normal" fontSize={{ sm: 12, md: 13, lg: 15 }}>
+              {data.socialMedia}
+            </Text>
+
+            <Text fontWeight="bold" fontSize={{ sm: 12, md: 13, lg: 15 }}>
+              Email: {data.email}
+            </Text>
+          </Flex>
+        </AccordionPanel>
+      </AccordionItem>
+    </Box>
+  );
+}
