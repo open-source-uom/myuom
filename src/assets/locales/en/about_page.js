ec3b9f4e945da83482ff59057d5ba3c63ca20932
George - David Apostolidis <iis20001@uom.edu.gr>
2023-09-08 18:40:01 +0300

    Version 1.2 change on About settings

--- src/assets/locales/en/about_page.js
+++ src/assets/locales/en/about_page.js
@@ -1,6 +1,6 @@
 export default {
     about_title: "myUoM Mobile App",
-    about_version: "Version V1.1.0",
+    about_version: "Version V1.2.0",
     about_find_us_on_socials: "Find us on Social Media",
     about_project_members_title: "Development Team Members"
 }
