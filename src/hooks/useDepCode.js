94b10d83fd401dc522ac31ce395f9f5293f18820
papsavas <papsavas56@gmail.com>
2024-02-11 19:41:13 +0200

    chore: simplify useDepCode

--- src/hooks/useDepCode.js
+++ src/hooks/useDepCode.js
@@ -2,7 +2,5 @@ import { useContext } from "react";
 import { DepartmentContext } from "../contexts/departmentContext";
 
 export const useDepCode = () => {
-  const { departmentCode, changeDepartmentCode } =
-    useContext(DepartmentContext);
-  return { departmentCode, changeDepartmentCode };
+  return useContext(DepartmentContext);
 };
