600031e6352abbe6831b901bf10f17458a72ae53
Piyush Agrawal <74711555+p1yu5h0@users.noreply.github.com>
2024-03-15 14:39:38 +0530

    Update src/assets/locales/el/error404.js
    Co-authored-by: Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>

--- src/assets/locales/el/error404.js
+++ src/assets/locales/el/error404.js
@@ -2,5 +2,5 @@
 export default {
     error_404: "Σφάλμα 404",
     page_not_found: "Η σελίδα δεν βρέθηκε",
-    go_to_homepage: "ΠΗΓΑΙΝΕ ΣΤΗΝ ΑΡΧΙΚΗ ΣΕΛΙΔΑ",
+    go_to_homepage: "Πήγαινε στην αρχική σελίδα",
 }
