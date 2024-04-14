6f4478342ff953433f076e5a7ac3096364e9cd3a
George Mellios <giostratakis@gmail.com>
2024-04-14 18:50:48 +0300

    Deleted comments/Cleanup

--- src/assets/data/announcements.js
+++ src/assets/data/announcements.js
@@ -59,29 +59,24 @@ export const AnnouncementsData = [
   {
     code: DEPARTMENT_CODES.INFORMATICS,
     link: "https://www.uom.gr/dai/ta-nea-toy-tmhmatos",
-    //not exist
 
   }, 
   {
     code: DEPARTMENT_CODES.BUSINESS_ADMINISTRATION,
     link: "https://www.uom.gr/ba",
-    //not updated
   }, 
   {
     code: DEPARTMENT_CODES.ECONOMICS,
     link: "https://www.uom.gr/eco#undefined1",
-    //not exist
   }, 
   {
     code: DEPARTMENT_CODES.ACCOUNTING_FINANCE,
     link: "https://www.uom.gr/fin",
-    //not updated
   }, 
   {
     code: DEPARTMENT_CODES.MUSIC,
     link: "https://www.uom.gr/msa",
     englishLink: "https://www.uom.gr/en/msa"
-    //almost not updated
   }
 
 ]
