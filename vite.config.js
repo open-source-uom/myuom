f55d59204a5c7bc042c4bd609a2fc1330b02e6af
Shivam Sharma <meshivam81@gmail.com>
2025-03-14 08:35:15 +0530

    feat: custom property and build step

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.md'],
+
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  }
 })
