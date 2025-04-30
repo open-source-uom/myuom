6d6c5c6f6d9fa85d2d424cdc4110f2df008ae384
adilkadivala <adilkadivala560@gmail.com>
2025-04-30 09:43:12 +0530

    auth.0 implemented successfully,  can easyly see user his/her detail

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,134 +36,205 @@
 
 */
 
-import { useEffect, useState } from "react";
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import myUOMLogo from "../assets/myUOMLogo.png";
-import { Flex, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react";
-import SettingsButton from "./settings/SettingsButton";
-import { Categories } from "../assets/ConfigRoutes";
-import { LeftArrowIcon } from "../assets/icons";
-import HomeIcon from "../assets/icons/HomeIcon";
-import i18n from "../i18n";
+import { useEffect, useState } from 'react'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import myUOMLogo from '../assets/myUOMLogo.png'
+import { Flex, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import SettingsButton from './settings/SettingsButton'
+import { Categories } from '../assets/ConfigRoutes'
+import { LeftArrowIcon } from '../assets/icons'
+import HomeIcon from '../assets/icons/HomeIcon'
+import i18n from '../i18n'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserDetail from './user-detail'
+
 function RouteDictionary(route) {
-  if (route === "/") return i18n.t("initial_page");
-  else {
-    for (let i = 0; i < Categories.length; i++) {
-      if (Categories[i].route === route) return Categories[i].title;
+    if (route === '/') return i18n.t('initial_page')
+    else {
+        for (let i = 0; i < Categories.length; i++) {
+            if (Categories[i].route === route) return Categories[i].title
+        }
     }
-  }
 }
 
 export default function Header() {
-  const navigate = useNavigate();
-  const loc = useLocation();
-  const [currentName, setCurrentName] = useState(RouteDictionary(loc.pathname));
-  useEffect(() => {
-    setCurrentName(RouteDictionary(`${loc.pathname}`));
-  }, [loc]);
+    const { loginWithRedirect, user, isAuthenticated, isLoading, logout } =
+        useAuth0()
 
-  const goToHomePage = () => {
-    navigate("/");
-  };
+    // user modal
+    const [openUserModal, setOpenUserModal] = useState(false)
 
-  return (
-    <>
-      <Flex
-        bgColor={useColorModeValue("#f3f3f3", "black")}
-        position={{ sm: "relative", lg: "sticky" }}
-        direction="row"
-        w="100%"
-        alignItems="center"
-        zIndex={99}
-        justifyContent="space-between"
-        padding="1rem"
-        top="0"
-      >
-        <Flex
-          w={{ sm: "100%", lg: "fit-content" }}
-          direction={{ sm: "column", lg: "row" }}
-          alignItems={{ sm: "start", lg: "center" }}
-          justifyContent="space-between"
-          gap={4}
-        >
-          <Flex
-            alignItems="center"
-            justifyContent="space-between"
-            w={{ sm: "100%", lg: "fit-content" }}
-          >
-            <Image
-              src={myUOMLogo}
-              onClick={goToHomePage}
-              cursor="pointer"
-              w={{ sm: "40px", xl: "60px" }}
-            />
-            <Box
-              display={{ sm: "block", lg: "none" }}
-              className={useColorModeValue("light-mode-svg", "dark-mode-svg")}
+    // modal handler
+    const handleUserModal = () => {
+        setOpenUserModal(!openUserModal)
+    }
+
+    const navigate = useNavigate()
+    const loc = useLocation()
+    const [currentName, setCurrentName] = useState(
+        RouteDictionary(loc.pathname)
+    )
+    useEffect(() => {
+        setCurrentName(RouteDictionary(`${loc.pathname}`))
+    }, [loc])
+
+    const goToHomePage = () => {
+        navigate('/')
+    }
+
+    return (
+        <>
+            <Flex
+                bgColor={useColorModeValue('#f3f3f3', 'black')}
+                position={{ sm: 'relative', lg: 'sticky' }}
+                direction="row"
+                w="100%"
+                alignItems="stretch"
+                zIndex={99}
+                justifyContent="space-between"
+                padding="1rem"
+                top="0"
             >
-              {loc.pathname !== "/" ? (
-                <Box
-                  cursor="pointer"
-                  onClick={goToHomePage}
-                  w={{ sm: "20px", lg: "20px", xl: "24px" }}
+                <Flex
+                    w={{ sm: '100%', lg: 'fit-content' }}
+                    direction={{ sm: 'column', lg: 'row' }}
+                    alignItems={{ sm: 'start', lg: 'center' }}
+                    justifyContent="space-between"
+                    gap={4}
                 >
-                  <LeftArrowIcon />
-                </Box>
-              ) : null}
-            </Box>
-          </Flex>
-          <Flex
-            className={useColorModeValue("light-mode-svg", "dark-mode-svg")}
-            direction="column"
-            alignItems="start"
-            justifyContent="start"
-            gap={1}
-          >
-            <Heading
-              fontSize={{ sm: 32, lg: 34 }}
-              color={useColorModeValue("black", "#f3f3f3")}
-              fontWeight={500}
-              fontFamily="Syne"
+                    <Flex
+                        alignItems="center"
+                        justifyContent="space-between"
+                        w={{ sm: '100%', lg: 'fit-content' }}
+                    >
+                        <Image
+                            src={myUOMLogo}
+                            onClick={goToHomePage}
+                            cursor="pointer"
+                            w={{ sm: '40px', xl: '60px' }}
+                        />
+                        <Box
+                            display={{ sm: 'block', lg: 'none' }}
+                            className={useColorModeValue(
+                                'light-mode-svg',
+                                'dark-mode-svg'
+                            )}
+                        >
+                            {loc.pathname !== '/' ? (
+                                <Box
+                                    cursor="pointer"
+                                    onClick={goToHomePage}
+                                    w={{ sm: '20px', lg: '20px', xl: '24px' }}
+                                >
+                                    <LeftArrowIcon />
+                                </Box>
+                            ) : null}
+                        </Box>
+                    </Flex>
+                    <Flex
+                        className={useColorModeValue(
+                            'light-mode-svg',
+                            'dark-mode-svg'
+                        )}
+                        direction="column"
+                        alignItems="start"
+                        justifyContent="start"
+                        gap={1}
+                    >
+                        <Heading
+                            fontSize={{ sm: 32, lg: 34 }}
+                            color={useColorModeValue('black', '#f3f3f3')}
+                            fontWeight={500}
+                            fontFamily="Syne"
+                        >
+                            {currentName}
+                        </Heading>
+                        {loc.pathname !== '/' ? (
+                            <Box
+                                cursor="pointer"
+                                onClick={goToHomePage}
+                                w={{ sm: '20px', lg: '20px', xl: '24px' }}
+                                display={{ sm: 'none', lg: 'block' }}
+                            >
+                                <LeftArrowIcon />
+                            </Box>
+                        ) : null}
+                    </Flex>
+                </Flex>
+                <Flex
+                    className={useColorModeValue(
+                        'light-mode-svg',
+                        'dark-mode-svg'
+                    )}
+                    alignItems="center"
+                    justifyContent="space-evenly"
+                    width={40}
+                >
+                    {isAuthenticated ? (
+                        <>
+                            <button
+                                onClick={handleUserModal}
+                                style={{
+                                    width: '40px',
+                                    height: '40px',
+                                    borderRadius: '50%',
+                                    overflow: 'hidden',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: '#0050E0',
+                                    color: '#fff',
+                                    fontWeight: 'bold',
+                                    border: 'none',
+                                    cursor: 'pointer',
+                                }}
+                            >
+                                {user?.name?.charAt(0).toUpperCase()}
+                            </button>
+
+                            {openUserModal && (
+                                <UserDetail user={user} logout={logout} />
+                            )}
+                        </>
+                    ) : (
+                        <button
+                            style={{
+                                padding: '10px',
+                                border: '1px solid #0050E0',
+                                borderRadius: '10px',
+                            }}
+                            onClick={() => loginWithRedirect()}
+                        >
+                            Log In
+                        </button>
+                    )}
+
+                    <Box display={{ sm: 'none', lg: 'block' }}>
+                        <SettingsButton />
+                    </Box>
+                </Flex>
+            </Flex>
+            <Flex
+                w="100%"
+                paddingX="20px"
+                zIndex={99}
+                height="60px"
+                alignItems="center"
+                justifyContent="space-around"
+                backgroundColor={useColorModeValue(
+                    'rgba(255, 255, 255, 1)',
+                    'rgba(0, 0, 0, 1)'
+                )}
+                display={{ sm: 'flex', lg: 'none' }}
+                position="fixed"
+                bottom="0"
             >
-              {currentName}
-            </Heading>
-            {loc.pathname !== "/" ? (
-              <Box
-                cursor="pointer"
-                onClick={goToHomePage}
-                w={{ sm: "20px", lg: "20px", xl: "24px" }}
-                display={{ sm: "none", lg: "block" }}
-              >
-                <LeftArrowIcon />
-              </Box>
-            ) : null}
-          </Flex>
-        </Flex>
-        <Box display={{ sm: "none", lg: "block" }}>
-          <SettingsButton />
-        </Box>
-      </Flex>
-      <Flex
-        w="100%"
-        paddingX="20px"
-        zIndex={99}
-        height="60px"
-        alignItems="center"
-        justifyContent="space-around"
-        backgroundColor={useColorModeValue(
-          "rgba(255, 255, 255, 1)",
-          "rgba(0, 0, 0, 1)"
-        )}
-        display={{ sm: "flex", lg: "none" }}
-        position="fixed"
-        bottom="0"
-      >
-        <Box onClick={goToHomePage}>
-          <HomeIcon />
-        </Box>
-        <SettingsButton />
-      </Flex>
-      <Outlet />
-    </>
-  );
+                <Box onClick={goToHomePage}>
+                    <HomeIcon />
+                </Box>
+                <SettingsButton />
+            </Flex>
+            <Outlet />
+        </>
+    )
 }
