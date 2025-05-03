93354449788640d1e4e1fe4dbc477c0911b1670e
GEORGIOS MOYSIADIS <georgemois23@gmail.com>
2025-05-03 21:23:44 +0300

    Fix broken link and icons
    Replace broken external service links with fallback URLs to uom.gr/

--- src/assets/data/Services.js
+++ src/assets/data/Services.js
@@ -40,8 +40,8 @@ export const servicesData = [
             },
             {
                 title: i18n.t("environmental_management_office"),
-                url: "http://www.perivpamak.gr/",
-
+                url: "https://www.uom.gr/emas",
+                englishUrl: "https://www.uom.gr/en/emas",
                 imgUrl:
                     "https://1.bp.blogspot.com/-t4PMium6v6g/X2x6Sz0aAFI/AAAAAAAAD4A/bJNjlS_kYr4lXGQdQBAD4mMdmrWEJ0qjgCNcBGAsYHQ/s2048/Logo%2B%25CE%25A0%25CE%25B5%25CF%2581%25CE%25B9%25CE%25B2%25CE%25B1%25CE%25BB%25CE%25BB%25CE%25BF%25CE%25BD%25CF%2584%25CE%25B9%25CE%25BA%25CE%25AE%25CF%2582.png",
             },
@@ -64,7 +64,7 @@ export const servicesData = [
             },
             {
                 title: "TedxUniversityofMacedonia",
-                url: "https://tedxuniversityofmacedonia.com/",
+                url: "https://www.facebook.com/TEDxUniversityofMacedonia",
                 imgUrl:
                     "https://yt3.googleusercontent.com/ytc/AOPolaSlS3zxQGiE3VqoN39ylXWldVLBujRdFij3z0G8-Q=s900-c-k-c0x00ffffff-no-rj",
             },
@@ -90,7 +90,7 @@ export const servicesData = [
                 title: i18n.t("cybersecurity_student_club"),
                 url: "https://csc.uom.gr/",
                 imgUrl:
-                    "https://csc.uom.gr/wp-content/uploads/2021/02/Myrmidones_02.jpg",
+                    "https://csc.uom.gr/wp-content/uploads/2024/11/CSC_Logo.png",
             },
         ],
     },
@@ -112,7 +112,7 @@ export const servicesData = [
             },
             {
                 title: i18n.t("gender_eq_committee"),
-                url: "https://www.uom.gr/eif",
+                url: "https://eif.uom.gr/",
                 englishUrl: "https://www.uom.gr/en/eif",
                 imgUrl:
                     "https://www.uom.gr/assets/site/public/nodes/8310/9033-EIFLOGO.jpg",
@@ -125,16 +125,16 @@ export const servicesData = [
             },
             {
                 title: i18n.t("accessibility_unit"),
-                url: "https://www.uom.gr/accessibility",
-                englishUrl: "https://www.uom.gr/en/accessibility",
+                url: "https://www.uom.gr/epitroph-isotimhs-prosbashs-atomon-me-anaphria-kai-atomon-me-eidikes-ekpaideytikes-anages",
+                englishUrl: "https://www.uom.gr/en/committee-for-equal-access-of-persons-with-disabilities-and-persons-with-special-educational-needs",
                 imgUrl:
                     "https://www.uom.gr/site/images/logo.png",
             },
             {
                 title: i18n.t("uni_publications"),
-                url: "https://www.uompress.gr/index.php/el/#",
-                
-                imgUrl: "https://www.uompress.gr/images/stories/logo_uom_v.png",
+                url: "https://www.uom.gr/uompress",
+                englishUrl: "https://www.uom.gr/en/uompress",
+                imgUrl: "https://www.uom.gr/site/images/logo.png",
             },
         ],
     },
